66c8e4bded3ffe25a67977ba1ce48de8eae3c582
KgothatsoTheko <kgothatsotheko7@gmail.com>
2025-03-21 07:04:11 +0200

    task 7 done, upload

--- Task-7.cpp
+++ Task-7.cpp
@@ -8,10 +8,44 @@
 //  display it to the user.
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
+
+    int subjects, i = 1;
+    double grade, sum = 0.0;
+    vector<double> mark;
+    string done = "Done, thank you;)";
+
+    cout << "Student Grade Calculator" << '\n';
+    cout << "Enter number of subjects: " << '\n';
+    cin >> subjects;
+    do
+    {
+        cout << "Enter mark of subject " << i << '\n';
+        cin >> grade;
+        mark.push_back(grade);
+        sum += grade;
+        i++;
+        /* code */
+    } while (i <= subjects);
+
+    cout << "\nMarks Entered:\n";
+
+    for(int marking : mark) {
+        cout << "marks: " << marking << '\n';
+    }
+
+    // Calculate average
+    double average = sum / subjects;
+    cout << "\nAverage Grade: " << average << '\n';
+
+    string comp = "comp, thank you;)";
+    cin >> comp;
+    
+
 
     return 0;
 }
